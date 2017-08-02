207ff287b692afa6034f3b55b9606d32e6b26841
Sahana Sripadanna <sripadanna.sahana@gmail.com>
2017-08-02 14:44:46 -0700

    displays list of restaurants and addresses

--- foursquare_api.js
+++ foursquare_api.js
@@ -23,20 +23,24 @@ $(document).ready(function() //waits for the document to all be ready before pro
     console.log(lat);
     console.log(longi);
     near = prompt("Location? ");
-    query = prompt("What type of food?");
+    near = near.replace(" ", "%20");
+    query = prompt("What type of food? (vegan, vegetarian, halal, donuts etc.)");
+    query = query.replace(' ', '%20');
     makeURL();
 	};
-	function error(err)
+
+
+  function error(err) // if the user declines to give loocal address
   {
 		console.warn('ERROR(' + err.code + '): ' + "We need your location to customize your results. Hit reload and hit allow activate location services.");
+    window.alert("We need your location to customize your results. Hit reload and hit allow to activate location services.");
 	};
 	navigator.geolocation.getCurrentPosition(success, error, {timeout: 10000});
 });
 
 
-function makeURL()
+function makeURL() // all the appends
 {
-  //all the appends + later, make it so that you choose between current location and entering your own also need options for price, time of day etc
   base_url += "&ll=" + lat + "," + longi;
   base_url += "&near=" + near;
   base_url += "&radius=" + radius;
@@ -46,4 +50,48 @@ function makeURL()
   base_url += "&venuePhotos=" + venuePhotos;
   base_url += "&sortByDistance" + sortByDistance;
   console.log(base_url);
+  setFourSquareArray();
 }
+
+
+//set base url to something else
+function setFourSquareArray(){
+  $.ajax({
+    type: 'GET',
+    url: base_url,
+    dataType: 'json',
+    crossDomain: true,
+
+    success: function(data){
+      console.log("i've successfully read the json code");
+
+
+      for (var i = 0; i < data.response.groups[0].items.length; i++) {
+        document.write(data.response.groups[0].items[i].venue.name + "</br>");
+        document.write(data.response.groups[0].items[i].venue.categories[0].shortName + "</br>");
+          var blegh = data.response.groups[0].items[i].venue.location.address;
+          console.log(blegh);
+          if (typeof blegh == "undefined"){
+            data.response.groups[0].items[i].venue.location.address = "Address unknown";
+          }
+        document.write(data.response.groups[0].items[i].venue.location.address + " ");
+        document.write(data.response.groups[0].items[i].venue.location.city + " ");
+        document.write(data.response.groups[0].items[i].venue.location.country + "</br>");
+//document.write(data.response.groups[0].items[i].reasons.items[0].summary + "</br>"); REASON: always = "this spot is popular"
+        document.write("</br>")
+        document.write("</br>")
+      }
+
+      console.log(data);
+    },
+    error: function(request, data, error)
+    {
+      console.log("dammit");
+    }
+  })
+
+
+}
+
+//use ajax
+
